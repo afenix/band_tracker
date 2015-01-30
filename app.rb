03b21572ee7ccea9b1092044f703db209041bdc3
Alister A. Fenix <a.fenix@icloud.com>
2015-01-30 14:40:35 -0800

    Add validation to ensure that user always enters a band or venue.

--- app.rb
+++ app.rb
@@ -10,12 +10,28 @@ get('/') do
 end
 
 post('/bands') do
-  Band.create(:name => params.fetch('name'))
-  redirect('/')
+  name = params.fetch('name')
+  venue = params.fetch('venue')
+  @band = Band.new({:name => name})
+  @venue = Venue.new({:name => venue})
+  @band.save()
+  @venue.save()
+  if @band.save()
+    erb(:success)
+  else
+    erb(:errors)
+  end
+  if @venue.save()
+    erb(:success)
+  else
+    erb(:errors)
+  end
 end
 
 get('/bands/:id') do
-  id = params.fetch('id').to_i()
-  @band = Band.find(id)
+  @band = Band.find(params.fetch("id"))
   erb(:band)
+end
+
+patch('/bands/:id/edit') do
 end
